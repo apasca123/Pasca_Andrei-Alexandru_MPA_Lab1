7499f994f02ff81767864dcd0200fc94595f12ec
apasca123 <45859349+apasca123@users.noreply.github.com>
2023-10-19 18:00:21 +0300

    Commit 1

---
+++ Pasca_Andrei-Alexandru_Lab1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Pasca_Andrei_Alexandru_Lab1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
